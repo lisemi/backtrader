0570be47d6914c88402f2b7ddb525b875761053b
sampson <lisemi@163.com>
2024-09-19 19:34:34 +0800

    完善firstcase实例

--- example/firstcase/firstcase.py
+++ example/firstcase/firstcase.py
@@ -1,3 +1,4 @@
+#%%
 import os
 import sys
 import pandas as pd
@@ -11,43 +12,113 @@ import tushare as ts
 # sys.path.insert(0, project_package_dir)
 import backtrader as bt
 
-
- 
-def get_data(code='600519',starttime='2017-01-01',endtime='2020-01-01'):
-    df = ts.get_k_data(code,start = starttime,end = endtime)
-    print(df)
-    df.index = pd.to_datetime(df.date)
-    df['openinterest'] = 0;
-    df = df[['open','high','low','close','volume','openinterest']]
+def get_data(code='600519.SH',starttime='20170101',endtime='20200101'):
+    ts.set_token('91c9159e9e4e8694ee81475ff5e0b3af938a0640f84f5fc964e6d5bb')
+    pro = ts.pro_api()
+    # tushare pro获取K线数据默认是倒序，即最新数据在最前面。所以这里需要倒回来。
+    df = pro.daily(ts_code=code, start_date=starttime, end_date=endtime).sort_values(by='trade_date', ascending=True)
+    df.index = pd.to_datetime(df.trade_date)
+    df['openinterest'] = 0
+    df = df[['open','high','low','close','vol','amount','openinterest']]
     return df
+ 
  
 stock_df =get_data()
 fromdate = datetime(2017,1,1)
 todate = datetime(2020,1,1)
 data = bt.feeds.PandasData(dataname=stock_df,fromdate=fromdate,todate=todate)
- 
+print(stock_df.head())
+ #%%
+
+ # 交易策略：当前价格大于当前移动平均值则卖出，否则买入
 class MyStrategy(bt.Strategy):
+    # 技术指标参数，可以在addstrategy添加策略的时候指定参数的值
     params=(
         ('maperiod',20),
     )
+
+    def log(self, txt, dt=None): 
+        # 记录策略的执行日志  
+        dt = dt or self.datas[0].datetime.date(0) 
+        print('%s, %s' % (dt.isoformat(), txt)) 
     
     def __init__(self):
+        # 保存收盘价的引用  
+        self.dataclose = self.datas[0].close 
         self.order = None
+        # 买入价格和手续费  
+        self.buyprice = None 
+        self.buycomm = None 
+        # 加入均线指标  
         self.ma = bt.indicators.SimpleMovingAverage(self.datas[0],period=self.params.maperiod)
+        # 绘制图形时候用到的指标  
+        bt.indicators.ExponentialMovingAverage(self.datas[0], period=25) 
+        bt.indicators.WeightedMovingAverage(self.datas[0], period=25,subplot=True) 
+        bt.indicators.StochasticSlow(self.datas[0]) 
+        bt.indicators.MACDHisto(self.datas[0]) 
+        rsi = bt.indicators.RSI(self.datas[0]) 
+        bt.indicators.SmoothedMovingAverage(rsi, period=10) 
+        bt.indicators.ATR(self.datas[0], plot=False) 
+
     def next(self):
-        if(self.order):
-            return
+        # 打印收盘价  
+        # self.log('Close, %.2f' % self.dataclose[0]) 
+        # 如果有订单正在挂起，不操作  
+        if self.order: 
+            return 
+
         if(not self.position):
-            if self.datas[0].close[0] > self.ma[0]:
-                self.order = self.sell (size=200)
-            else:
-                if self.datas[0].close[0] < self.ma[0]:
+            if self.dataclose[0] < self.ma[0]:
+                if self.dataclose[-1] < self.ma[-1]:
+                    self.log('买入单, %.2f' % self.dataclose[0]) 
                     self.order = self.buy(size=200)
+        else:
+            if self.dataclose[0] > self.ma[0]:
+                if self.dataclose[-1] > self.ma[-1]:
+                    self.log('卖出单, %.2f' % self.dataclose[0]) 
+                    self.order = self.sell (size=200)
+
+    def notify_order(self, order): 
+        if order.status in [order.Submitted, order.Accepted]:
+            # broker 提交/接受了，买/卖订单则什么都不做  
+            return 
+        # 检查一个订单是否完成  
+        # 注意: 当资金不足时，broker会拒绝订单  
+        if order.status in [order.Completed]: 
+            if order.isbuy(): 
+                self.log( 
+                    '已买入, 价格: %.2f, 费用: %.2f, 佣金 %.2f' % 
+                    (order.executed.price, 
+                    order.executed.value, 
+                    order.executed.comm)) 
+                self.buyprice = order.executed.price 
+                self.buycomm = order.executed.comm 
+            elif order.issell(): 
+                self.log('已卖出, 价格: %.2f, 费用: %.2f, 佣金 %.2f' % 
+                    (order.executed.price, 
+                    order.executed.value, 
+                    order.executed.comm)) 
+            # 记录当前交易数量  
+            self.bar_executed = len(self) 
+
+        elif order.status in [order.Canceled, order.Margin, order.Rejected]: 
+            self.log('订单取消/保证金不足/拒绝')
+
+        # 其他状态记录为：无挂起订单  
+        self.order = None 
+
+    # 交易状态通知，一买一卖算交易  
+    def notify_trade(self, trade): 
+        if not trade.isclosed: 
+            return 
+        self.log('交易利润, 毛利润 %.2f, 净利润 %.2f' % 
+            (trade.pnl, trade.pnlcomm)) 
+        
  
 cerebro = bt.Cerebro()
 cerebro.adddata(data)
 cerebro.addstrategy(MyStrategy)
-startcash = 100000
+startcash = 1000000
 cerebro.broker.setcash(startcash)
 cerebro.broker.setcommission(0.0002)
  
@@ -57,3 +128,6 @@ print(f"初始资金：{startcash}\n回测时间:{s}-{t}")
 cerebro.run()
 portval = cerebro.broker.getvalue()
 print(f"剩余总资金：{portval}\n回测时间:{s}-{t}")
+cerebro.plot()
+# cerebro.plot(iplot=False, style='candlestick', barup='red', bardown='green', volume=True, volup='red', voldown='green')
+# %%
--- example/lesson/Lesson1.py
+++ example/lesson/Lesson1.py
@@ -9,6 +9,7 @@ import datetime
 # 实例化 cerebro
 cerebro = bt.Cerebro()
 
+# 将制定列解析为日期的方式读取文件内容
 daily_price = pd.read_csv("Data/daily_price.csv", parse_dates=['datetime'])
 trade_info = pd.read_csv("Data/trade_info.csv", parse_dates=['trade_date'])
 #%%
@@ -18,13 +19,13 @@ for stock in daily_price['sec_code'].unique():
     # 日期对齐
     data = pd.DataFrame(daily_price['datetime'].unique(), columns=['datetime'])  # 获取回测区间内所有交易日
     df = daily_price.query(f"sec_code=='{stock}'")[
-        ['datetime', 'open', 'high', 'low', 'close', 'volume', 'openinterest']]
+        ['datetime', 'open', 'high', 'low', 'close', 'volume', 'openinterest']]  # 查询股票代码等于stock的股票，并提取括号指定的列
     data_ = pd.merge(data, df, how='left', on='datetime')
     data_ = data_.set_index("datetime")
     # print(data_.dtypes)
     # 缺失值处理：日期对齐时会使得有些交易日的数据为空，所以需要对缺失数据进行填充
     data_.loc[:, ['volume', 'openinterest']] = data_.loc[:, ['volume', 'openinterest']].fillna(0)
-    data_.loc[:, ['open', 'high', 'low', 'close']] = data_.loc[:, ['open', 'high', 'low', 'close']].fillna(method='pad')
+    data_.loc[:, ['open', 'high', 'low', 'close']] = data_.loc[:, ['open', 'high', 'low', 'close']].fillna(method='pad')  # 使用前一个有效值来填充 NaN
     data_.loc[:, ['open', 'high', 'low', 'close']] = data_.loc[:, ['open', 'high', 'low', 'close']].fillna(0)
     # 导入数据
     datafeed = bt.feeds.PandasData(dataname=data_, fromdate=datetime.datetime(2019, 1, 2),
@@ -90,6 +91,7 @@ class TestStrategy(bt.Strategy):
             dt = dt or self.datas[0].datetime.date(0)
             print(f'{dt.isoformat()},{txt}')
 
+    # 获得订单状态变化的通知，只要next触发了买卖操作，就会有通知订单消息
     def notify_order(self, order):
         # 未被处理的订单
         if order.status in [order.Submitted, order.Accepted]:
@@ -122,10 +124,21 @@ cerebro.broker.setcommission(commission=0.0003)
 # 滑点：双边各 0.0001
 cerebro.broker.set_slippage_perc(perc=0.005)
 
-cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='pnl')  # 返回收益率时序数据
-cerebro.addanalyzer(bt.analyzers.AnnualReturn, _name='_AnnualReturn')  # 年化收益率
-cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='_SharpeRatio')  # 夏普比率
-cerebro.addanalyzer(bt.analyzers.DrawDown, _name='_DrawDown')  # 回撤
+# 添加图表设置
+cerebro.addobserver(bt.observers.Broker)
+cerebro.addobserver(bt.observers.Trades)
+cerebro.addobserver(bt.observers.DrawDown)
+
+# 添加分析指标
+cerebro.addanalyzer(bt.analyzers.Returns, _name='_Returns')             # 收益率
+cerebro.addanalyzer(bt.analyzers.TimeReturn, _name='_TimeReturns')      # 返回收益率时序数据。收益率期间
+cerebro.addanalyzer(bt.analyzers.AnnualReturn, _name='_AnnualReturn')   # 年化收益率
+cerebro.addanalyzer(bt.analyzers.SharpeRatio, _name='_SharpeRatio',     # 计算年化夏普比率
+                    timeframe=bt.TimeFrame.Days,
+                    annualize=True, riskfreerate=0)
+cerebro.addanalyzer(bt.analyzers.DrawDown, _name='_DrawDown')           # 回撤
+cerebro.addanalyzer(bt.analyzers.TimeDrawDown, _name='_TimeDrawDown')   # 期间回撤
+cerebro.addanalyzer(bt.analyzers.TradeAnalyzer, _name='_TradeAnalyzer') # 交易统计信息，如获胜、失败次数等
 
 # 将编写的策略添加给大脑，别忘了 ！
 cerebro.addstrategy(TestStrategy, printlog=True)
@@ -135,13 +148,19 @@ result = cerebro.run()
 # 从返回的 result 中提取回测结果
 strat = result[0]
 # 返回日度收益率序列
-daily_return = pd.Series(strat.analyzers.pnl.get_analysis())
+daily_return = pd.Series(strat.analyzers._TimeReturns.get_analysis())
 # 打印评价指标
-print("--------------- AnnualReturn -----------------")
+print("--------------- 收益期间 -----------------")
+print(strat.analyzers._TimeReturns.get_analysis())
+print("--------------- 年化收益率 ---------------")
 print(strat.analyzers._AnnualReturn.get_analysis())
-print("--------------- SharpeRatio -----------------")
-print(strat.analyzers._SharpeRatio.get_analysis())
-print("--------------- DrawDown -----------------")
+print("--------------- 最大回撤 -----------------")
 print(strat.analyzers._DrawDown.get_analysis())
+print('\n')
+print('收 益 率: ', strat.analyzers._Returns.get_analysis()['rtot'])
+print('夏普比率: ', strat.analyzers._SharpeRatio.get_analysis()['sharperatio'])
+print(f'最终资金: {cerebro.broker.getvalue():,.2f} 元')
+
+cerebro.plot(iplot=False, style='candlestick', barup='red', bardown='green', volume=True, volup='red', voldown='green')
 
 
